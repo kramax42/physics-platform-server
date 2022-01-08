5aa43f5aebe777a355d15f84ea3992b6b05a5e4a
max99xam <pepetrov123@list.ru>
2022-01-08 23:10:55 +0300

    added more types

--- src/server.ts
+++ src/server.ts
@@ -5,9 +5,6 @@ var addon = require("../../napi-addon-fdtd/build/Release/napi-addon-fdtd.node");
 
 import { CONTINUE, PAUSE, START, CLOSE } from "../constants/ws-event.constants";
 
-let intervalId;
-let condition: number[];
-
 import {
   LAB_1_2D,
   LAB_2_3D,
@@ -15,29 +12,74 @@ import {
   LAB_4_DIFRACTION,
 } from "../constants/data-type.constants";
 
-type dataType = "2D" | "3D" | "INTERFERENCE" | "DIFRACTION";
-let currentDataType: dataType;
-let refractionMatrix: number[];
-let refractionMatrixRows: number;
-
-type eventTypes = "start" | "pause" | "continue" | "close";
-
-type startMessageType = {
-  event: eventTypes;
-  type: dataType;
-  dataToReturn: dataToReturnType;
-  condition: number[];
-  matrix: number[][];
-};
-
-let getData;
-
-let tempReturnData: number;
-type dataToReturnType = "Ez" | "Hy" | "Hx" | "Energy";
-let dataToReturn: dataToReturnType = "Ez";
+import {
+  dataToReturnType,
+  dataType,
+  InitDataObjectType,
+  startMessageType,
+} from "../types/types";
 
 const port = 5000;
 
+let intervalId;
+
+const startSendingDataInit = (
+  message: startMessageType
+): InitDataObjectType => {
+  
+  // Unpacking request data.
+  const currentDataType: dataType = message.type;
+
+  let returnDataNumber: number;
+
+  let getData;
+  let dataToReturn: dataToReturnType;
+
+  switch (currentDataType) {
+    case LAB_1_2D:
+      getData = addon.getFdtd2D;
+      break;
+    case LAB_2_3D:
+      getData = addon.getFDTD_3D;
+      break;
+    case LAB_3_INTERFERENCE:
+      getData = addon.getFDTD_3D_INTERFERENCE;
+      break;
+    case LAB_4_DIFRACTION:
+      getData = addon.getFDTD_3D_DIFRACTION;
+      dataToReturn = message.dataToReturn;
+      switch (dataToReturn) {
+        case "Ez":
+          returnDataNumber = 0;
+          break;
+        case "Hy":
+          returnDataNumber = 1;
+          break;
+        case "Hx":
+          returnDataNumber = 2;
+          break;
+        case "Energy":
+          returnDataNumber = 3;
+          break;
+        default:
+          returnDataNumber = 0;
+      }
+      break;
+  }
+
+  return {
+    condition: message.condition,
+    currentDataType,
+    refractionMatrix: message.matrix.flat(),
+    refractionMatrixRows: message.matrix.length,
+    returnDataNumber,
+    dataToReturn,
+    returnDataStr: "data" + dataToReturn,
+    getData,
+  };
+};
+
+// Websocket server entry point.
 const wss = new ws.Server(
   {
     port,
@@ -45,111 +87,78 @@ const wss = new ws.Server(
   () => console.log(`Server started on ${port}`)
 );
 
+let obj: InitDataObjectType;
+
+// Websocket handlers.
 wss.on("connection", function connection(ws) {
   ws.on("message", async function (messageJSON) {
     const message: startMessageType = JSON.parse(messageJSON);
+
     switch (message.event) {
       case START:
         // Clear previous process.
         clearInterval(intervalId);
         console.log("start sending data");
 
-        condition = message.condition;
-        currentDataType = message.type;
-        refractionMatrix = message.matrix.flat();
-        refractionMatrixRows = message.matrix.length;
-        console.log("refractionMatrix: ", refractionMatrix);
+        obj = startSendingDataInit(message);
 
-        switch (currentDataType) {
-          case LAB_1_2D:
-            getData = addon.getFdtd2D;
-            break;
-          case LAB_2_3D:
-            getData = addon.getFDTD_3D;
-            break;
-          case LAB_3_INTERFERENCE:
-            getData = addon.getFDTD_3D_INTERFERENCE;
-            break;
-          case LAB_4_DIFRACTION:
-            getData = addon.getFDTD_3D_DIFRACTION;
-            dataToReturn = message.dataToReturn;
-            switch (dataToReturn) {
-              case "Ez":
-                tempReturnData = 0;
-                break;
-              case "Hy":
-                tempReturnData = 1;
-                break;
-              case "Hx":
-                tempReturnData = 2;
-                break;
-              case "Energy":
-                tempReturnData = 3;
-                break;
-              default:
-                tempReturnData = 0;
-            }
-            break;
-        }
-        newInterval(true, ws.send.bind(ws));
+        newInterval(true, ws.send.bind(ws), obj);
         break;
       case PAUSE:
         clearInterval(intervalId);
         console.log("pause sending data");
         break;
       case CONTINUE:
-        newInterval(false, ws.send.bind(ws));
+        newInterval(false, ws.send.bind(ws), obj);
         console.log("continue sending data");
         break;
       case CLOSE:
         clearInterval(intervalId);
-        // addon.clearFDTD_2D_data();
         console.log("stop sending data | connection closed");
-
         break;
+      default:
+        clearInterval(intervalId);
+        console.log("Wrong request data!");
     }
   });
 });
 
 // Milliseconds.
-const TIME_INTERVAL = 500;
+const TIME_INTERVAL = 650;
 
-async function newInterval(reload: boolean, send) {
+async function newInterval(reload: boolean, send, obj?: InitDataObjectType) {
   intervalId = null;
 
-  let data = await getData(
-    condition,
+  // Initial data request.
+  let data = await obj.getData(
+    obj.condition,
     reload,
-    refractionMatrix,
-    refractionMatrixRows,
-    tempReturnData
+    obj.refractionMatrix,
+    obj.refractionMatrixRows,
+    obj.returnDataNumber
   );
 
   const stepsPerInterval = 5;
+  const reloadInInterval = false;
   intervalId = setInterval(async () => {
     for (let j = 0; j < stepsPerInterval; ++j) {
-      data = await getData(
-        condition,
-        false,
-        refractionMatrix,
-        refractionMatrixRows,
-        tempReturnData
+      data = await obj.getData(
+        obj.condition,
+        reloadInInterval,
+        obj.refractionMatrix,
+        obj.refractionMatrixRows,
+        obj.returnDataNumber
       );
     }
 
-    // console.log("data.dataX", data.dataX);
-    // console.log(data["data" + dataToReturn]);
-    // console.log("data.row", data.row);
-    // console.log("refractionMatrixRows", refractionMatrixRows);
-
-    data = {
+    const dataToClient = {
       dataX: data.dataX,
       dataY: data.dataY,
-      dataVal: data["data" + dataToReturn],
+      dataVal: data[obj.returnDataStr],
       step: data.currentTick,
       row: data.row,
       col: data.col,
     };
-    send(JSON.stringify(data));
+    send(JSON.stringify(dataToClient));
   }, TIME_INTERVAL);
 }
---
+++ types/types.ts
@@ -0,0 +1,42 @@
+export type dataType = "2D" | "3D" | "INTERFERENCE" | "DIFRACTION";
+export type eventType = "start" | "pause" | "continue" | "close";
+
+export type dataToReturnType = "Ez" | "Hy" | "Hx" | "Energy";
+
+export type startMessageType = {
+  event: eventType;
+  type: dataType;
+  dataToReturn: dataToReturnType;
+  condition: number[];
+  matrix: number[][];
+};
+
+export type ReturnObjAddonType = {
+  dataX: number[][];
+  dataY: number[][];
+  dataEz?: number[][];
+  dataHy?: number[][];
+  dataHx?: number[][];
+  currentTick: number;
+  row: number;
+  col: number;
+};
+
+export type GetDataType = (
+  condition: number[],
+  reload: boolean,
+  refractionMatrix: number[],
+  refractionMatrixRows: number,
+  returnDataNumber: number
+) => ReturnObjAddonType;
+
+export type InitDataObjectType = {
+  condition: number[];
+  returnDataNumber: number;
+  currentDataType: dataType;
+  refractionMatrix: number[];
+  dataToReturn: dataToReturnType;
+  returnDataStr: string;
+  getData: GetDataType;
+  refractionMatrixRows: number;
+};
