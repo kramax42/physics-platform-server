9fe1d6aa9f57cb3e0191a11799ab499ac543229a
max99xam <pepetrov123@list.ru>
2022-02-10 19:42:22 +0300

    added correct build

---
+++ src/server.ts
@@ -0,0 +1,200 @@
+import { Server } from 'ws';
+
+import * as addon from 'napi-addon-fdtd';
+
+import { CONTINUE, PAUSE, START, CLOSE } from "../constants/ws-event.constants";
+
+import {
+  LAB_2D,
+  LAB_3D,
+  LAB_3D_INTERFERENCE,
+} from "../constants/data-type.constants";
+
+import {
+  dataToReturnType,
+  dataType,
+  InitDataObjectType,
+  startMessageType,
+} from "../types/types";
+ 
+
+
+
+
+const port = process.env.PORT || 5001;
+
+let intervalId;
+
+const startSendingDataInit = (
+  message: startMessageType
+): InitDataObjectType => {
+
+  // Unpacking request data.
+  const currentDataType: dataType = message.type;
+
+  let returnDataNumber: number;
+
+  let getData;
+  let dataToReturn: dataToReturnType;
+
+  switch (currentDataType) {
+    
+    case LAB_2D:
+      getData = addon.getFdtd2D;
+      break;
+
+    case LAB_3D:
+      console.log("3d!!!!!!!")
+      getData = addon.getFDTD_3D_DIFRACTION;
+      dataToReturn = message.dataToReturn;
+      switch (dataToReturn) {
+
+        case "Ez":
+          returnDataNumber = 0;
+          break;
+
+        case "Hy":
+          returnDataNumber = 1;
+          break;
+
+        case "Hx":
+          returnDataNumber = 2;
+          break;
+
+        case "Energy":
+          returnDataNumber = 3;
+          break;
+
+        default:
+          returnDataNumber = 0;
+      }
+      break;
+      case LAB_3D_INTERFERENCE:
+        console.log("INTERFERNCE")
+      getData = addon.getFDTD_3D_INTERFERENCE;
+      dataToReturn = message.dataToReturn;
+      switch (dataToReturn) {
+
+        case "Ez":
+          returnDataNumber = 0;
+          break;
+
+        case "Hy":
+          returnDataNumber = 1;
+          break;
+
+        case "Hx":
+          returnDataNumber = 2;
+          break;
+
+        case "Energy":
+          returnDataNumber = 3;
+          break;
+
+        default:
+          returnDataNumber = 0;
+      }
+      break;
+  }
+
+  return {
+    condition: message.condition,
+    currentDataType,
+    refractionMatrix: message.matrix?.flat(),
+    refractionMatrixRows: message.matrix?.length,
+    returnDataNumber,
+    dataToReturn,
+    returnDataStr: "data" + dataToReturn,
+    getData,
+  };
+};
+
+// Websocket server entry point.
+const wss = new Server(
+  {
+    port,
+  },
+  () => console.log(`Server started on ${port}`)
+);
+
+let obj: InitDataObjectType;
+
+// Websocket handlers.
+wss.on("connection", function connection(ws) {
+  ws.on("message", async function (messageJSON) {
+    const message: startMessageType = JSON.parse(messageJSON);
+
+    switch (message.event) {
+      case START:
+        // Clear previous process.
+        clearInterval(intervalId);
+        console.log("start sending data");
+        obj = startSendingDataInit(message);
+        newInterval(true, ws.send.bind(ws), obj);
+        break;
+
+      case PAUSE:
+        clearInterval(intervalId);
+        console.log("pause sending data");
+        break;
+
+      case CONTINUE:
+        newInterval(false, ws.send.bind(ws), obj);
+        console.log("continue sending data");
+        break;
+
+      case CLOSE:
+        clearInterval(intervalId);
+        console.log("stop sending data | connection closed");
+        break;
+
+      default:
+        clearInterval(intervalId);
+        console.log("Wrong request data!");
+    }
+  });
+});
+
+// Milliseconds.
+const TIME_INTERVAL = 650;
+
+async function newInterval(reload: boolean, send, obj?: InitDataObjectType) {
+  intervalId = null;
+
+  // Initial data request.
+  let data = await obj.getData(
+    obj.condition,
+    reload,
+    obj.refractionMatrix,
+    obj.refractionMatrixRows,
+    obj.returnDataNumber
+  );
+
+  const stepsPerInterval = 5;
+  const reloadInInterval = false;
+  intervalId = setInterval(async () => {
+
+    for (let j = 0; j < stepsPerInterval; ++j) {
+      data = await obj.getData(
+        obj.condition,
+        reloadInInterval,
+        obj.refractionMatrix,
+        obj.refractionMatrixRows,
+        obj.returnDataNumber
+      );
+    }
+
+    const dataToClient = {
+      dataX: data.dataX,
+      dataY: data.dataY,
+      dataVal: data[obj.returnDataStr],
+      step: data.currentTick,
+      row: data.row,
+      col: data.col,
+    };
+    send(JSON.stringify(dataToClient));
+  }, TIME_INTERVAL);
+}
+
+
+// const  SERVER_URL="ws://physics-platform-server.herokuapp.com/"
