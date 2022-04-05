33f535f799aa71913bef9d4b01e4965cc4340ad0
max99xam <pepetrov123@list.ru>
2022-04-05 23:05:00 +0300

    .

--- dist/constants/ws-event.constants.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CLOSE = exports.CONTINUE = exports.PAUSE = exports.START = void 0;
-exports.START = 'start';
-exports.PAUSE = 'pause';
-exports.CONTINUE = 'continue';
-exports.CLOSE = 'close';
-//# sourceMappingURL=ws-event.constants.js.map
--- node_modules/napi-addon-fdtd/src/globals.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <math.h>
-
-static const size_t Nx = 400;
-static const size_t Ny = 400;
