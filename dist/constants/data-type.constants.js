43327b18c5d66ab5bd8a38526988878e94e47a0a
max99xam <pepetrov123@list.ru>
2022-04-06 18:16:56 +0300

    .

--- dist/constants/data-type.constants.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.LAB_3D_INTERFERENCE = exports.LAB_3D = exports.LAB_2D = void 0;
-exports.LAB_2D = '2D';
-exports.LAB_3D = '3D';
-exports.LAB_3D_INTERFERENCE = 'INTERFERENCE';
-//# sourceMappingURL=data-type.constants.js.map
