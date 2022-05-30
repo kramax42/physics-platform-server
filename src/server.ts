be86359d2a8adfdcb6b7128e16a13b986fb9d2d3
max99xam <pepetrov123@list.ru>
2022-05-30 13:45:24 +0300

    updated api doc

--- src/server.ts
+++ src/server.ts
@@ -15,9 +15,27 @@ app.get('/', (req,res) => res.send((`<pre>
 LabName:        Params:
 ------------------------------------------------------------------
 
-getData2D:             ( [lambda, tau, refractive_index], reload )
-INTERFERENCE:   ( [lambda, beamsize], reload )
-getData3D:     ( [lambda, beamsize], reload, matrix, matrixSize, dataReturnType ) -- 4 - data return type('Ez' = 0 | 'Hy' = 1 |'Hx' = 2 |'Energy' = 3)
+getData1D: (
+  [omega, tau],
+  reload,
+  material vector,
+  size ,
+  epsilin array,
+  mu array,
+  sigma array,
+  relative source position array )
+
+getData2D: (
+   [lambda, beamsize],
+   reload,
+   material matrix (flat),
+   size (size x size),
+   epsilin array,
+   mu array,
+   sigma array,
+   data return type(number)   ('Ez' = 0 | 'Hy' = 1 |'Hx' = 2 |'Energy' = 3),
+   relative source position array )
+
 --------------------------------------------------------------
 
 synchronize step:    ( step: number )
